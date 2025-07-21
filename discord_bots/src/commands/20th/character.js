f1f2ca8272712db0584ae878e3155f03b44b5c66
Mirai-Miki <Mirai-Miki@hotmail.com>
2025-07-21 12:54:26 +1000

    Update supporter requirement text in character commands
    Changed the description for the 'default' subcommand in both 20th and 5th edition character command files to clarify supporter requirements, replacing 'Requires Mortal+ Supporter Tier' with 'Supporter Required'.

--- discord_bots/src/commands/20th/character.js
+++ discord_bots/src/commands/20th/character.js
@@ -66,7 +66,7 @@ function getCommands() {
         subcommand
           .setName("default")
           .setDescription(
-            "Set a default character for this server for dice rolls and updates. [Requires Mortal+ Supporter Tier]"
+            "Set a default character for this server for dice rolls and updates. [Supporter Required]"
           )
 
           .addStringOption((option) =>
